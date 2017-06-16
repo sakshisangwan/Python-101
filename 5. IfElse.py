1938618f3d8e10c046c3a18ada670ad3adcccc76
Pradhvan Bisht <Pradhvanbisht@gmail.com>
2017-06-16 16:25:49 +0530

    deleted

--- 5. IfElse.py
+++
@@ -1,6 +0,0 @@
-# To check whether a number is greater than 100 or not 
-number = int (input("Enter an integer input: "))
-if number < 100:
-	print "Number is smaller than 100"
-else:
-	print "Number is greater than 100"
